a181cbf1fa1c40e720c8bf1ed5408c6749fc8d00
Petar Paar <pepaar@microsoft.com>
2015-09-21 23:42:48 +0200

    ContentPage

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,12 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import React = require("react");
+import ContentPage from "./Components/ContentPage/ContentPage";
 
 export default class App extends React.Component<{}, {}> {
     render(): React.ReactElement<{}> {
         return  <div>
-                    Hello!
+                    <ContentPage />
                 </div>;
     }
 };
---
+++ App/Components/ContentPage/ContentPage.tsx
@@ -0,0 +1,9 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+import React = require("react");
+
+export default class App extends React.Component<{}, {}> {
+    render(): React.ReactElement<{}> {
+        return  <div>Conent!</div>;
+    }
+};
