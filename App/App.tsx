1e956de0ce7db7d5435bedd55f7ff6490e408530
Petar Paar <pepaar@microsoft.com>
2016-04-03 20:49:50 +0200

    Use BaseComponent in App

--- App/App.tsx
+++ App/App.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import ContentPage from "./Components/ContentPage/ContentPage";
 import * as CommonActionCreators from "./ActionCreators/CommonActionCreators";
+import BaseComponent from "./Components/BaseComponent";
 require("./Log/ActionLogger");
 require("./Global/Styles/global.less");
 
-export default class App extends React.Component<{}, {}> {
-    render(): React.ReactElement<{}> {
+export default class App extends BaseComponent<{}, {}> {
+    doRender(): React.ReactElement<{}> {
         return  <div>
                     <ContentPage />
                 </div>;
