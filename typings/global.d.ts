a4c965e0582af0a1a3fb8c0f177a9b2d8772c915
Petar Paar <pepaar@microsoft.com>
2017-04-12 15:39:17 +0200

    Use @types/webpack-env

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,10 +1,3 @@
-declare const DEBUG: boolean;
-declare const module: IHotModule;
-
-interface IHotModule {
-  hot?: { accept: (path: string, callback: () => void) => void };
-}
-
 interface Window {
     __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
 }
